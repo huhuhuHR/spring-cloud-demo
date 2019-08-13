63a80819cda7ff876adb8dabb32154f8c1579316
huorong <757033301@qq.com>
2019-08-14 07:22:03 +0800

    网关超时研究

---
+++ image-server/src/main/java/com/github/imageserver/ImageServerApplication.java
@@ -0,0 +1,16 @@
+package com.github.imageserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author huorong
+ */
+@SpringBootApplication
+public class ImageServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageServerApplication.class, args);
+    }
+
+}
