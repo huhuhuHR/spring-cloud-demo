9f3abb60bf6fa4cb0a2b032b7494d4d68f7c4858
huorong <huorong@unicloud.com>
2019-09-23 19:43:15 +0800

    springboot初始化结束执行的方法

---
+++ python-datax-web/DataxWeb.py
@@ -0,0 +1,28 @@
+from flask import Flask
+import py_eureka_client.eureka_client as eureka_client
+
+app = Flask(__name__)
+
+
+def setEureka():
+    server_host = "10.0.50.52"
+    server_port = 5000
+    eureka_client.init(eureka_server="http://localhost:1111/eureka",
+                       app_name="datax_service",
+                       # 当前组件的主机名，可选参数，如果不填写会自动计算一个，如果服务和 eureka 服务器部署在同一台机器，请必须填写，否则会计算出 127.0.0.1
+                       instance_host=server_host,
+                       instance_port=server_port,
+                       # 调用其他服务时的高可用策略，可选，默认为随机
+                       ha_strategy=eureka_client.HA_STRATEGY_RANDOM)
+
+
+setEureka();
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True, threaded=True, port=5000, host="10.0.50.52")
